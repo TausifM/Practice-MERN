3ada1fc0d6c28a29f735c92f626b9d187db6e5bf
TausifM <nisargcontact2020@gmail.com>
2022-01-26 14:25:57 +0530

    passes custom data by req.query.dataname

--- nodejs/express-server.js
+++ nodejs/express-server.js
@@ -1,3 +1,4 @@
+const { application } = require("express");
 const express = require("express");
 const app = express();
 app.get("/", (req, res) => {
@@ -5,8 +6,28 @@ app.get("/", (req, res) => {
   res.send("get stated express server " + req.query.name);
   console.log("started server");
 });
+// localhost:5202/?name="tausif"
 // if data is send by user in client by req.query.name as it is query
 app.get("/about", (req, res) => {
   res.send("about route by express");
+});
+app.get("/input", (req, res) => {
+  res.send(`
+  <input type="text" placeholder="enter" value="${req.query.name}"/>
+  <button>click</button> 
+  <a href="/about" >About Page</a>
+  `);
+});
+app.get("/json", (req, res) => {
+  res.send([
+    {
+      name: "tausif",
+      lastName: "sheikh",
+    },
+    {
+      name: "xyz",
+      lastName: "sheikh",
+    },
+  ]);
 });
 app.listen(5202);
