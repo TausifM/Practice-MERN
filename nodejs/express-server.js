7656e0dd66b0e69125eb09e5af4e744fbd619872
TausifM <nisargcontact2020@gmail.com>
2022-01-26 15:02:53 +0530

    404 page route

--- nodejs/express-server.js
+++ nodejs/express-server.js
@@ -19,29 +19,29 @@ app.get("/aboutme", (_, res) => {
 app.get("*", (_, res) => {
   res.sendFile(`${publicPath}/404.html`);
 });
-// app.get("/about", (req, res) => {
-//   res.send("about route by express");
-// });
+app.get("/about", (req, res) => {
+  res.send("about route by express");
+});
 
-// app.get("/input", (req, res) => {
-//   res.send(`
-//   <input type="text" placeholder="enter" value="${req.query.name}"/>
-//   <button>click</button>
-//   <a href="/about" >About Page</a>
-//   `);
-// });
+app.get("/input", (req, res) => {
+  res.send(`
+  <input type="text" placeholder="enter" value="${req.query.name}"/>
+  <button>click</button>
+  <a href="/about" >About Page</a>
+  `);
+});
 
-// app.get("/json", (req, res) => {
-//   res.send([
-//     {
-//       name: "tausif",
-//       lastName: "sheikh",
-//     },
-//     {
-//       name: "xyz",
-//       lastName: "sheikh",
-//     },
-//   ]);
-// });
+app.get("/json", (req, res) => {
+  res.send([
+    {
+      name: "tausif",
+      lastName: "sheikh",
+    },
+    {
+      name: "xyz",
+      lastName: "sheikh",
+    },
+  ]);
+});
 
 app.listen(5202);
