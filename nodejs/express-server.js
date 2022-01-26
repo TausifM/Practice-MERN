30a209cc623c0b764e354e238b8e80c8fab6930e
TausifM <nisargcontact2020@gmail.com>
2022-01-26 14:43:04 +0530

    HTML pages

--- nodejs/express-server.js
+++ nodejs/express-server.js
@@ -1,23 +1,30 @@
-const { application } = require("express");
 const express = require("express");
+const path = require("path");
+
 const app = express();
-app.get("/", (req, res) => {
+const publicPath = path.join(__dirname, "public");
+app.use(express.static(publicPath));
+
+app.get("", (req, res) => {
   console.log("data send by browser", req.query.name);
   res.send("get stated express server " + req.query.name);
   console.log("started server");
 });
 // localhost:5202/?name="tausif"
 // if data is send by user in client by req.query.name as it is query
+
 app.get("/about", (req, res) => {
   res.send("about route by express");
 });
+
 app.get("/input", (req, res) => {
   res.send(`
   <input type="text" placeholder="enter" value="${req.query.name}"/>
-  <button>click</button> 
+  <button>click</button>
   <a href="/about" >About Page</a>
   `);
 });
+
 app.get("/json", (req, res) => {
   res.send([
     {
@@ -30,4 +37,5 @@ app.get("/json", (req, res) => {
     },
   ]);
 });
+
 app.listen(5202);
