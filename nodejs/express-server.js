56ed564f8df524e7bad557c866a080242a91edc8
TausifM <nisargcontact2020@gmail.com>
2022-01-26 14:14:50 +0530

    request query

--- nodejs/express-server.js
+++ nodejs/express-server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 app.get("/", (req, res) => {
-  res.send("get stated express server");
+  console.log("data send by browser", req.query.name);
+  res.send("get stated express server " + req.query.name);
   console.log("started server");
 });
+// if data is send by user in client by req.query.name as it is query
 app.get("/about", (req, res) => {
   res.send("about route by express");
 });
